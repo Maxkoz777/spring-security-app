659f171b35b281d4ae2b6c9d89494c65c4190874
Maxim <maxkozhinov777@gmail.com>
2021-02-10 17:39:46 +0300

    added DB (MySQL) for keeping info about accounts

--- src/main/java/com/maxkoz777/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/maxkoz777/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,28 +1,38 @@
 package com.maxkoz777.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // add a reference to our security data source
+
+    @Autowired
+    private DataSource securityDataSource;
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        // use jdbc authentication ... oh yeah!!!
 
-        auth.inMemoryAuthentication().withUser(users.username("Max").password("1234").roles("MANAGER", "EMPLOYEE"))
-                                    .withUser(users.username("James").password("5678").roles("EMPLOYEE"))
-                                    .withUser(users.username("Mary").password("3456").roles("ADMIN", "EMPLOYEE"));
+        auth.jdbcAuthentication().dataSource(securityDataSource);
 
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http.authorizeRequests()
                 .antMatchers("/").hasRole("EMPLOYEE")
                 .antMatchers("/leaders/**").hasRole("MANAGER")
@@ -33,10 +43,16 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginProcessingUrl("/authenticateTheUser")
                 .permitAll()
                 .and()
-                .logout()
-                .permitAll()
+                .logout().permitAll()
                 .and()
-                .exceptionHandling()
-                .accessDeniedPage("/access-denied");
+                .exceptionHandling().accessDeniedPage("/access-denied");
+
     }
+
 }
+
+
+
+
+
+
